46cfdea89d34c39b7515420be61f8943d3a0c11f
sam-cruz21 <iamsam.cruz1@gmail.com>
2024-01-28 22:01:14 -0600

    Added Variables

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-console.log('What is you favorite color?');
+var life = 100; //This is the life bar according to the video NUMBER
+
+life= life - 10;
+
+//life is equal to 90
+
+life= life - 10;
+
+//life is equal to 80
+
+var name = 'color?' // This the the name displayed in the game
+
+
+//block variable//
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }
 
 
 
 
+
+
+
+
+console.log(life);
