fb2a161c6da978d5b455cc8b2ebb9f488ec701b1
sam-cruz21 <iamsam.cruz1@gmail.com>
2024-02-06 16:04:29 -0600

    Update app.js
    Added examples of functions and parameters

--- app.js
+++ app.js
@@ -1,27 +1,81 @@
-var life = 100; //This is the life bar according to the video NUMBER
-
-life= life - 10;
-
-//life is equal to 90
-
-life= life - 10;
-
-//life is equal to 80
-
-var name = 'color?' // This the the name displayed in the game
 
 
-//block variable//
-function myFunction() {
-    document.getElementById("demo1").innerHTML = "Hello!";
-    document.getElementById("demo2").innerHTML = "How are you?";
+//function//
+const name= "Dev Ed"
+
+function logger() {
+  console.log("Party Time");
+  console.log("Party Time");
+  console.log("Party Time");
+  console.log("Party Time");
   }
 
+logger(); //function invocation//
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//example 1: function with parameter//
+const name = "Dev Ed"
 
+function adder(number1, number2)  {
+console.log(numeber1 + number2);
+}
 
+adder(5,10);
 
+//example 2//
+const name = "Dev Ed";
 
-console.log(life);
+function toUpper(text) {
+  const upperCased = text.toUpperCase();
+  console.log(upperCased);
+}
+toUpper(name);
+
+//example 3//
+const cake ="Tres Leches"
+
+function toUpper(text) {
+  const upperCased = text.toUpperCase();
+  console.log(upperCased);
+}
+toUpper(cake);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//object with property//
+const person = {
+  firstName: "Samantha",
+  lastName: "Cruz",
+  id: 5566,
+  fullName: function() {
+    return this.FirstName + "" + this.lastName
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Event//
+
+<button onclick="alert('Hello')">Click me.</button> ///"onclick" is the event
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Example 1:Method//
+let x = 150;
+x.toString();
+(150).toString();
+(100 + 50).toString();
+
+//example 2//
+let x = 9.656;
+x.toExponential(2);
+x.toExponential(4);
+x.toExponential(6);
+
+//example 3//
+let x = 9.656;
+x.toFixed(0);
+x.toFixed(2);
+x.toFixed(4);
+x.toFixed(6);
