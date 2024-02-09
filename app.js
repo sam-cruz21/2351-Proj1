ecb9e4a59143b2724751775e00dae977533b0a25
sam-cruz21 <iamsam.cruz1@gmail.com>
2024-02-09 16:25:48 -0600

    number and string method

--- app.js
+++ app.js
@@ -1,81 +1,59 @@
-
-
-//function//
-const name= "Dev Ed"
-
-function logger() {
-  console.log("Party Time");
-  console.log("Party Time");
-  console.log("Party Time");
-  console.log("Party Time");
-  }
-
-logger(); //function invocation//
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//example 1: function with parameter//
-const name = "Dev Ed"
-
-function adder(number1, number2)  {
-console.log(numeber1 + number2);
-}
-
-adder(5,10);
+//Example 1 concatenation string variable//
+console.log("Hello my name is" + " Sam");  //concatenation string variable//
 
 //example 2//
-const name = "Dev Ed";
+const name = "Samantha";
+const age = 21;
 
-function toUpper(text) {
-  const upperCased = text.toUpperCase();
-  console.log(upperCased);
+console.log("Hello it's me " + name + " and my age is " + age + " and I am getting old" );
+
+//another way to write the string above//
+console.log("Hello it's me ${name} and my age is ${age}");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//"if" conditional statement//
+
+const age = 21;
+
+if(age > 18) {
+  console.log ("You are good to go!");
+} else {
+  console.log("You aren't old enough");
 }
-toUpper(name);
 
-//example 3//
-const cake ="Tres Leches"
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function toUpper(text) {
-  const upperCased = text.toUpperCase();
-  console.log(upperCased);
-}
-toUpper(cake);
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//string method//
+let text = "abcdefgh";
+let length = text.length;// the outcome counts the length of the characters, the output would be 8
 
-//object with property//
-const person = {
-  firstName: "Samantha",
-  lastName: "Cruz",
-  id: 5566,
-  fullName: function() {
-    return this.FirstName + "" + this.lastName
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//switch statement//
+switch(fruits) {
+  
+  case
+   "Banana":
+      alert("Hello")
+      break;
+    
+  case
+   "Apple":
+      alert("Leave")
+      break;    
   }
-};
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //Number Method//
 
-//Event//
-
-<button onclick="alert('Hello')">Click me.</button> ///"onclick" is the event
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//Example 1:Method//
-let x = 150;
-x.toString();
-(150).toString();
-(100 + 50).toString();
-
-//example 2//
-let x = 9.656;
-x.toExponential(2);
-x.toExponential(4);
-x.toExponential(6);
-
-//example 3//
-let x = 9.656;
-x.toFixed(0);
-x.toFixed(2);
-x.toFixed(4);
-x.toFixed(6);
+Number(true);
+Number(false);
+Number("10");
+Number("  10");
+Number("10  ");
+Number(" 10  ");
+Number("10.33");
+Number("10,33");
+Number("10 33");
+Number("John");//number method converts variables to numbers//
