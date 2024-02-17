e0d967ba3f42127d7cd2660245ba58d09f1a2e0f
sam-cruz21 <iamsam.cruz1@gmail.com>
2024-02-17 15:02:47 -0600

    redid functions and parameters

--- app.js
+++ app.js
@@ -1,59 +1,40 @@
-//Example 1 concatenation string variable//
-console.log("Hello my name is" + " Sam");  //concatenation string variable//
 
-//example 2//
-const name = "Samantha";
-const age = 21;
-
-console.log("Hello it's me " + name + " and my age is " + age + " and I am getting old" );
-
-//another way to write the string above//
-console.log("Hello it's me ${name} and my age is ${age}");
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//"if" conditional statement//
-
-const age = 21;
-
-if(age > 18) {
-  console.log ("You are good to go!");
-} else {
-  console.log("You aren't old enough");
+//function that passes parameters in and out//
+function addNumbers(num1, num2) {  //addNumbers is the function invocation//
+  return num1 + num2;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var result = addNumbers(5, 3);
+console.log(result); // output is 8//
+
+//An object with multiple properties//
+var person = {
+  firstName: "Sam",
+  lastName: "Cruz",
+  age: 21,
+  gender: "female",
+  email: "iamsam.cruz1@gmail.com"
+};
+
+// Accessing properties from object
+console.log("First Name:", person.firstName);
+console.log("Last Name:", person.lastName);
+console.log("Age:", person.age);
+console.log("Gender:", person.gender);
+console.log("Email:", person.email);
+
 
 //string method//
-let text = "abcdefgh";
-let length = text.length;// the outcome counts the length of the characters, the output would be 8
+var str = "hello world";
+var upperCaseStr = str.toUpperCase();
+console.log(upperCaseStr); // Output is HELLO WORLD
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//event method//
+var button = document.getElementById("myButton");
 
-//switch statement//
-switch(fruits) {
-  
-  case
-   "Banana":
-      alert("Hello")
-      break;
-    
-  case
-   "Apple":
-      alert("Leave")
-      break;    
-  }
+// Adding a click event listener to the button
+button.addEventListener("click", function() {         //click is the event//
+    alert("Button clicked!");
+});
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //Number Method//
 
-Number(true);
-Number(false);
-Number("10");
-Number("  10");
-Number("10  ");
-Number(" 10  ");
-Number("10.33");
-Number("10,33");
-Number("10 33");
-Number("John");//number method converts variables to numbers//
