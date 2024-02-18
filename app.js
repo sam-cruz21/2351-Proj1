a466d01ed6238eb83afe8880cff3dfcb65374979
sam-cruz21 <iamsam.cruz1@gmail.com>
2024-02-17 20:51:55 -0600

    Loops and DOM

--- app.js
+++ app.js
@@ -1,60 +1,24 @@
-//concatenated variable//
-var firstName = "Sam";
-var lastName = "C";
 
-var fullName = firstName + " " + lastName;
-console.log(fullName); // Output is Sam C
+//DOM CSS//
+const text = document. querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
+const title = document.querySelector(".marvel");
+const spurs = document.querySelector(".spurs");
 
-//if conditional statement//
-var temperature = 65;
+text.classList.add("change");
+title.classList.add("change");
+marvel.classList.add("change");
+spurs.classList.add("change");
 
-if (temperature > 50) {
-    console.log("It's a little warm outside");
-} else if (temperature >= 45 && temperature <= 55) {
-    console.log("It's a nice day.");
-} else {
-    console.log("It's cold outside.");
-}
-//switch statement//
-var dayOfWeek = "Monday";
-var schedule;
-
-switch (dayOfWeek) {
-    case "Monday":
-        schedule = "Get groceries for the week at 9AM";
-        break;
-    case "Tuesday":
-        schedule = "Schedule a doctor's appointment after 12PM ";
-        break;
-    case "Wednesday":
-        schedule = "Go to the gym at 10PM";
-        break;
-    case "Thursday":
-        schedule = "Pay light bill after work at 7PM";
-        break;
-    case "Friday":
-        schedule = "Last minute cleaning in the evening";
-        break;
-    default:
-        schedule = "Nothing planned for today";
+//loop for//
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
 }
 
-console.log("Today's schedule:", schedule);
+//loop while//
+let i = 1;
 
-//String method//
-String.prototype.toUppercase = function() {
-  return this.toUpperCase();
-};
-
-//output
-var str = "Taylor Swift's music is basic";
-console.log(str.toUppercase()); // Output: "TAYLOR SWIFT'S MUSIC IS BASIC"
-
-//Number Method//
-Number.prototype.customToString = function() {
-  return this.toString();
-};
-
-var num = 210;
-console.log(num.customToString()); // Output is "210"
-
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
