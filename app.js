78470ed01225fc32e1232fd79166ffd832ed8d5d
sam-cruz21 <157275436+sam-cruz21@users.noreply.github.com>
2024-03-07 13:48:21 -0600

    Beginner Project 3
    Hover

--- app.js
+++ app.js
@@ -1,35 +1,13 @@
-let data = [
-    {
-        name: "Samantha",
-        age: "21"
-    },
-    {
-        name: "Fernando",
-        age: "20"
-    },   
-    {
-        name: "Candy",
-        age: "20"
-    },   
-    {
-        name: "Isme",
-        age: "21"
-    },   
-    {
-        name: "Zee",
-        age: "20"
-    },   
-    {
-        name: "Felipe",
-        age: "21"
-    },
-    
-];
+const circle = document.querySelector("#circle")
 
-const info = document.querySelector("#info")
+circle.addEventListener("mouseenter", () => {
+    if(!circle.classList.contains("hover")) {
+        circle.classList.add("hover");
+    }
+} );
 
-let details  = data.map(function (item) {
-return "<div>" + item.name + " " + "is " + item.age + " years old" + "</div>";
+circle.addEventListener("mouseleave", () => {
+    if(circle.classList.contains ("hover")) {
+        circle.classList.remove ("hover");
+    }
 });
-
-info.innerHTML = details.join("\n");
