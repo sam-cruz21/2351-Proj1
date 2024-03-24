418ead76efa7fa68d1dba0f04cb99fc5d82aab31
sam-cruz21 <157275436+sam-cruz21@users.noreply.github.com>
2024-03-24 14:56:30 -0500

    Add files via upload
    APIs

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-const circle = document.querySelector("#circle")
+//Browser APIs//
+const x = document.getElementById("demo");
 
-circle.addEventListener("mouseenter", () => {
-    if(!circle.classList.contains("hover")) {
-        circle.classList.add("hover");
-    }
-} );
+function getLocation() {
+  try {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } catch {
+    x.innerHTML = err;
+  }
+}
 
-circle.addEventListener("mouseleave", () => {
-    if(circle.classList.contains ("hover")) {
-        circle.classList.remove ("hover");
-    }
-});
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+
+function myFunction() {
+    const inpObj = document.getElementById("id1");
+    if (!inpObj.checkValidity()) {
+      document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    } else {
+      document.getElementById("demo").innerHTML = "Input OK";
+    } 
+  } 
+
+  function myFunction() {
+    let text;
+    if (document.getElementById("id1").validity.rangeOverflow) {
+      text = "Value too large";
+    } else {
+      text = "Input OK";
+    } 
+    document.getElementById("demo").innerHTML = text;
+  }
+
+  function myFunction() {
+    let text;
+    if (document.getElementById("id1").validity.rangeUnderflow) {
+      text = "Value too small";
+    } else {
+      text = "Input OK";
+    } 
+    document.getElementById("demo").innerHTML = text;
+  }
+
+  localStorage.setItem("name","Sammy C");
+  document.getElementById("demo").innerHTML = localStorage.getItem("name");
+
+  
